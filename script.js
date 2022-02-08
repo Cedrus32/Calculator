be16a95e9f5d2cdd058da9f759adb89f8665b28b
Shannon <srosenb@protonmail.ch>
2022-02-08 05:49:22 -0600

    Add working mult() and partly working div()

--- script.js
+++ script.js
@@ -2,7 +2,8 @@
 // MATH FUNCTIONS //
 // -------------- //
 
-// ...add -- WORKING with whole nums (+/-) && decimals (+/-) && 2+ inputs
+//...add
+//WORKING with whole nums (+/-) && decimals (+/-) && 2+ inputs
 function add(...nums) {
     let sum = nums.reduce((total, number) => {
         return total += number;
@@ -10,7 +11,8 @@ function add(...nums) {
     return sum;
 }
 
-// ...sub -- WORKING with whole nums (+/-) && decimals (+/-) && 2+ inputs
+//...sub
+//WORKING with whole nums (+/-) && decimals (+/-) && 2+ inputs
 function sub(...nums) {
     let sum = nums.reduce((total, number) => {
         return total -= number;
@@ -18,13 +20,27 @@ function sub(...nums) {
     return sum;
 }
 
-// function multiply() {
-    
-// }
+//...mult
+//WORKING with whole nums (+/-) && decimals (+/-) && 2+ inputs
+function mult(...nums) {
+    let sum = nums.reduce((total, number) => {
+        return total *= number;
+    });
+    return sum;
+}
 
-// function divide() {
-    
-// }
+//...div
+//WORKING with whole nums (+/-) && decimals (+/-) && 2+ inputs && 'error' with 0 denominator
+//NOT WORKING with 0 denominator mid-array
+function div(...nums) {
+    let sum = nums.reduce((total, number) => {
+        if (number === 0) {
+            return 'error';
+        }
+        return total /= number;
+    });
+    return sum;
+}
 
 // ------- //
 // TESTING //
@@ -35,3 +51,9 @@ function sub(...nums) {
 
 // subTest = sub(10, -.5, 15);
 // console.log({subTest});
+
+// multTest = mult(2, 2.5, 5);
+// console.log({multTest});
+
+// divTest = div(2, 0, 2);
+// console.log({divTest});
