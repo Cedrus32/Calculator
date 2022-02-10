87e27928b1a918fd00764e8c06a91a71e011ef53
Shannon <srosenb@protonmail.ch>
2022-02-10 06:03:50 -0600

    Create dynamic display (current) WORKING -- todo: format display

--- script.js
+++ script.js
@@ -8,34 +8,51 @@
 //TODO .html --> <link ... dark.css>
 //TODO .js --> toggle dark
 
+//...switch theme...
 function switchTheme(src) {
     const themeCSS = document.querySelectorAll('.theme-CSS');
     themeCSS[1].href = src;
 }
 
+//switch to light theme...
 const themeButtons = document.querySelectorAll('.theme');
 themeButtons[0].addEventListener('click', () => {
     switchTheme('./light.css');
 });
 
+//switch to dark theme...
 themeButtons[1].addEventListener('click', () => {
     switchTheme('./dark.css');
 });
 
-// ------ //
-// KEYPAD //
-// ------ //
 
-//TODO store button.id in array --> to pass to display & functions
+// ---------------- //
+// DISPLAY & KEYPAD //
+// ---------------- //
 
+//TODO scrub array so display is accurate
+//get display elements
+let displayValues = [];
+const displayCurrent = document.querySelector('.current');
+
+//get & store number IDs --> send to display...
 const buttonNums = document.querySelectorAll('.num');
 buttonNums.forEach(button => button.addEventListener('click', () => {
-    console.log(button.id);
+    displayValues.push(button.id);
+    // console.log(button.id);
+    console.log(displayValues);
+    //TODO link to display here
+    displayCurrent.textContent = displayValues;
 }));
 
+//get & store function IDs --> send to display...
 const buttonFuncts = document.querySelectorAll('.function');
 buttonFuncts.forEach(button => button.addEventListener('click', () => {
-    console.log(button.id);
+    displayValues.push(button.id);
+    // console.log(button.id);
+    console.log(displayValues);
+    //TODO link to display here
+    displayCurrent.textContent = displayValues;
 }))
 
 //TODO clear --> clear value array && display
